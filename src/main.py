beeb3f44283bb5a3dbfa9f84e47c2c3f0b43fa6a
Карина Бородина <kadrinko@gmail.com>
2023-07-03 14:02:15 +0300

    Добавлены все функции
    close #1

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 import modules.perimetr as prm
+import modules.factorial as fct
 
 p1 = prm.perimetr_func([1,2,3])
 print(p1)
 
 p2 = prm.perimetr_func([1,2,3,5,6])
 print(p2)
+
+f1 = fct.fact_func(10)
+print(f1)
+
+f2 = fct.fact_func(5)
+print(f2)
 
 print("hello")
